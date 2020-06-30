a3bad31353a020b94c29ebf14cfab57c6fa47aff
David Kirsch <davidjkirsch@gmail.com>
2020-06-30 15:28:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,10 +1,11 @@
+require 'pry'
 # Code your solution here!
 def run_guessing_game
   picked_number = rand(6) +1 
   
   puts "Guess my number. It is a number between 1 and 6"
   input = gets.chomp
-  
+  binding.pry
   if(input == 'exit')
     puts "Goodbye!"
   end
